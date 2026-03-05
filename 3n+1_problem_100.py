3c9dd919e959cd43d5ba2a3bebd861976190e063
Tacotornado <james.mejia.73@gmail.com>
2026-03-05 14:58:41 +0800

    updating problem 100

---
+++ 3n+1_problem_100.py
@@ -0,0 +1,29 @@
+# idea: two numbers are run throuhh
+'''
+Considerthefollowingalgorithm:
+1. input n
+2. print n
+3. if n=1 then STOP
+4. if n is odd then n←−3n+1
+5. else n←−n/2
+6. GOTO2
+
+'''
+def algo(x):
+    sols = [x]
+    print(x)
+    while True:
+        if x == 1:
+            break
+                   
+        
+    
+
+def main():
+    # input logic:
+    
+    while True:
+        
+        
+        break    
+
