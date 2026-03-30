6bc5739eaaab2ca78499d99751dde5330e6469a4
Tacotornado <james.mejia.73@gmail.com>
2026-03-30 23:10:33 +0800

    saving changes made to airport express

--- Airport_express_11374.py
+++ Airport_express_11374.py
@@ -1,4 +1,5 @@
-import numpy as np
+import heapq
+
 
 def main():
     # input handling 
@@ -9,9 +10,34 @@ def main():
         if not line.strip():
             break
         
-        #first line input
+        #first line input, defining number of edges, Start, End
         N, S, E = map(int, line.split())
         
-        #second line input
-        while True:
-            sub_line = input()
+        eco = [[] for _ in range(N+1)]
+        com = [[] for _ in range(N+1)]
+        
+        # second line, adding in economy Xpress edges
+        M = int(input())
+        for _ in range(M):
+            x, y, z = map(int, input().split())
+            eco[x].append((y, z))
+            eco[y].append((x, z))
+            
+        K = int(input())
+        for _ in range(K):
+            x, y, z = map(int, input().split())
+            com[x].append((y, z))
+            com[y].append((x, z))
+            
+        
+        print("Eco points")    
+        for point in range(0, N):
+            for v, w in eco[point]:
+                print(f"{point} -> {v} (cost {w})")
+        
+        print("Com points")    
+        for point in range(0, N):
+            for v, w in com[point]:
+                print(f"{point} -> {v} (cost {w})")
+            
+main()
